4adb613bd3f14e869e464c7cf3290c1df6015ea9
Yunus Floo <yunus@floo.id>
2022-08-05 15:04:07 +0700

    setup android ci cd

--- lib/presentation/pages/receipt/screen/scan_printer_screen.dart
+++ lib/presentation/pages/receipt/screen/scan_printer_screen.dart
@@ -119,67 +119,12 @@ class ScanPrinterScreen extends StatelessWidget {
     bytes += generator.text('Terimakasih', styles: const PosStyles(bold: true, align: PosAlign.center));
     bytes += generator.feed(1);
     bytes += generator.text('Made with Alapos', styles: const PosStyles(align: PosAlign.center));
-    // bytes += generator.cut();
-    bytes += generator.feed(1);
+    bytes += generator.cut();
 
     var printer = BluePrint();
     printer.setData(bytes);
     await printer.printData(device);
     // device.disconnect();
-  }
-
-  void printWithDevice(BluetoothDevice device) async {
-    await device.connect();
-
-    final profile = await CapabilityProfile.load();
-    final generator = Generator(PaperSize.mm80, profile);
-    List<int> bytes = [];
-
-    bytes += generator.text('Regular: aA bB cC dD eE fF gG hH iI jJ kK lL mM nN oO pP qQ rR sS tT uU vV wW xX yY zZ');
-    bytes += generator.text('Special 1: àÀ èÈ éÉ ûÛ üÜ çÇ ôÔ', styles: const PosStyles(codeTable: 'CP1252'));
-    bytes += generator.text('Special 2: blåbærgrød', styles: const PosStyles(codeTable: 'CP1252'));
-
-    bytes += generator.text('Bold text', styles: const PosStyles(bold: true));
-    bytes += generator.text('Reverse text', styles: const PosStyles(reverse: true));
-    bytes += generator.text('Underlined text', styles: const PosStyles(underline: true), linesAfter: 1);
-    bytes += generator.text('Align left', styles: const PosStyles(align: PosAlign.left));
-    bytes += generator.text('Align center', styles: const PosStyles(align: PosAlign.center));
-    bytes += generator.text('Align right', styles: const PosStyles(align: PosAlign.right), linesAfter: 1);
-
-    bytes += generator.row([
-      PosColumn(
-        text: 'col3',
-        width: 3,
-        styles: const PosStyles(align: PosAlign.center, underline: true),
-      ),
-      PosColumn(
-        text: 'col6',
-        width: 6,
-        styles: const PosStyles(align: PosAlign.center, underline: true),
-      ),
-      PosColumn(
-        text: 'col3',
-        width: 3,
-        styles: const PosStyles(align: PosAlign.center, underline: true),
-      ),
-    ]);
-
-    bytes += generator.text('Text size 200%',
-        styles: const PosStyles(
-          height: PosTextSize.size2,
-          width: PosTextSize.size2,
-        ));
-
-    // Print barcode
-    final List<int> barData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 4];
-    bytes += generator.barcode(Barcode.upcA(barData));
-
-    bytes += generator.feed(2);
-    bytes += generator.cut();
-    var printer = BluePrint();
-    printer.setData(bytes);
-    await printer.printData(device);
-    device.disconnect();
   }
 
   @override
@@ -319,7 +264,10 @@ class BluePrint {
   ) async {
     for (var i = 0; i < data.length; i++) {
       try {
-        await charac.write(data[i]);
+        await charac.write(
+          data[i],
+          withoutResponse: true,
+        );
       } catch (e) {
         return false;
       }
